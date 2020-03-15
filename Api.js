9a2cb89801e740326e00073824f6c4d627195e63
HusseinMohamed13 <husseinmohamed067@gmail.com>
2020-03-15 11:37:35 +0200

    commit on 'master'

---
+++ Api.js
@@ -0,0 +1,25 @@
+
+async function makeRequest(co) {
+    const axios = require('axios');
+   
+    const config = {
+        method: 'get',
+        url: 'http://api.openweathermap.org/data/2.5/weather?q=+'+co+'&appid=c544f992a7963efe309415b840747537'   
+    }
+
+    let res = await axios(config)
+    return res
+    
+}
+
+const getcountryname = ()=>{
+    
+    return makeRequest('LONDON').then(res => {
+        const name = res.data.sys.country;
+        console.log('Country name of London is '+name);
+        return name;
+     });
+     
+};
+
+getcountryname();
